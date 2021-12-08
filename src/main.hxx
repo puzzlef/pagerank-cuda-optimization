bf597756c791b43d4413603058e0213427f51a5d
Subhajit Sahu <wolfram77@gmail.com>
2021-12-08 21:33:06 +0530

    :turtle: update src

--- src/main.hxx
+++ src/main.hxx
@@ -5,11 +5,23 @@
 #include "edges.hxx"
 #include "csr.hxx"
 #include "mtx.hxx"
+#include "snap.hxx"
 #include "copy.hxx"
 #include "transpose.hxx"
+#include "deadEnds.hxx"
+#include "selfLoop.hxx"
 #include "dfs.hxx"
 #include "components.hxx"
 #include "topologicalSort.hxx"
+#include "dynamic.hxx"
 #include "pagerank.hxx"
-#include "pagerankNvgraph.hxx"
+#include "pagerankSeq.hxx"
 #include "pagerankCuda.hxx"
+#include "pagerankMonolithicSeq.hxx"
+#include "pagerankMonolithicCuda.hxx"
+
+#ifndef NVGRAPH_DISABLE
+#include "pagerankNvgraph.hxx"
+#else
+#define pagerankNvgraph pagerankCuda
+#endif
