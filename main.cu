1e63a7b37935e21f443aed371d64d8e703fb52d1
Subhajit Sahu <wolfram77@gmail.com>
2021-12-10 22:57:18 +0530

    :turtle: w/ expt

--- main.cu
+++ main.cu
@@ -8,41 +8,33 @@ using namespace std;
 
 
 
-#define REPEAT 5
-#define SKIP_CHAINS true
-
 template <class G, class H>
-void runPagerank(const G& x, const H& xt, bool show) {
-  vector<float> *init  = nullptr;
-
-  // Find chains.
-  auto ch = chains(x, xt);
-  printf("chains: %d chain-vertices: %d {}\n", size(ch), size2d(ch));
-
-  // Find pagerank using nvGraph.
-  auto a1 = pagerankNvgraph(xt, init, {REPEAT});
-  auto e1 = l1Norm(a1.ranks, a1.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankNvgraph\n", a1.time, a1.iterations, e1);
+void runPagerank(const G& x, const H& xt, int repeat) {
+  enum NormFunction { L0=0, L1=1, L2=2, Li=3 };
+  vector<float> *init = nullptr;
 
   // Find pagerank without optimization.
-  auto a2 = pagerankCuda(x, xt, init, {REPEAT});
-  auto e2 = l1Norm(a2.ranks, a1.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda\n", a2.time, a2.iterations, e2);
+  auto a1 = pagerankMonolithicCuda(x, xt, init, {repeat, L1});
+  auto e1 = l1Norm(a1.ranks, a1.ranks);
+  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda\n", a1.time, a1.iterations, e1);
 
   // Find pagerank skipping rank calculation of chain vertices.
-  auto a3 = pagerankCuda(x, xt, init, {REPEAT, SKIP_CHAINS});
-  auto e3 = l1Norm(a3.ranks, a1.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [skip]\n", a3.time, a3.iterations, e3);
+  for (int SC=2; SC<=256; SC*=2) {
+    auto ch = chainsFromSize(x, xt, SC);
+    auto a2 = pagerankMonolithicCuda(x, xt, init, {repeat, L1, SC});
+    auto e2 = l1Norm(a2.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [skip-chains=%03d; chain-vertices=%08d; chains=%08d]\n", a2.time, a2.iterations, e2, SC, size2d(ch), size(ch));
+  }
 }
 
 
 int main(int argc, char **argv) {
   char *file = argv[1];
-  bool  show = argc > 2;
+  int repeat = argc>2? stoi(argv[2]) : 5;
   printf("Loading graph %s ...\n", file);
   auto x  = readMtx(file); println(x);
   auto xt = transposeWithDegree(x); print(xt); printf(" (transposeWithDegree)\n");
-  runPagerank(x, xt, show);
+  runPagerank(x, xt, repeat);
   printf("\n");
   return 0;
 }
