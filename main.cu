6499f27b61b6d16d97fe92b390b06c587b3365e2
Subhajit Sahu <wolfram77@gmail.com>
2021-12-11 00:37:07 +0530

    :turtle: w/ expt

--- main.cu
+++ main.cu
@@ -9,45 +9,39 @@ using namespace std;
 
 
 
-#define REPEAT 5
-
 template <class G, class H>
-void runPagerank(const G& x, const H& xt, bool show) {
+void runPagerank(const G& x, const H& xt, int repeat) {
+  enum NormFunction { L0=0, L1=1, L2=2, Li=3 };
   vector<float> *init = nullptr;
 
-  // Find pagerank using nvGraph.
-  auto a1 = pagerankNvgraph(xt, init, {REPEAT});
-  auto e1 = l1Norm(a1.ranks, a1.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankNvgraph\n", a1.time, a1.iterations, e1);
-
   // Find pagerank without optimization.
-  auto a2 = pagerankCuda(xt, init, {REPEAT});
-  auto e2 = l1Norm(a2.ranks, a1.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda\n", a2.time, a2.iterations, e2);
+  auto a1 = pagerankMonolithicCuda(x, xt, init, {repeat, L1});
+  auto e1 = l1Norm(a1.ranks, a1.ranks);
+  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda\n", a1.time, a1.iterations, e1);
 
   // Find pagerank skipping converged vertices with re-check.
   for (int skipCheck=2; skipCheck<16; skipCheck+=int(log2(skipCheck))) {
-    auto a3 = pagerankCuda(xt, init, {REPEAT, skipCheck, 0});
-    auto e3 = l1Norm(a3.ranks, a1.ranks);
-    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [skip-check=%d]\n", a3.time, a3.iterations, e3, skipCheck);
+    auto a2 = pagerankMonolithicCuda(x, xt, init, {repeat, L1, skipCheck, 0});
+    auto e2 = l1Norm(a2.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [skip-check=%d]\n", a2.time, a2.iterations, e2, skipCheck);
   }
 
   // Find pagerank skipping converged vertices after several turns.
   for (int skipAfter=2; skipAfter<64; skipAfter+=int(log2(skipAfter))) {
-    auto a4 = pagerankCuda(xt, init, {REPEAT, 0, skipAfter});
-    auto e4 = l1Norm(a4.ranks, a1.ranks);
-    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [skip-after=%d]\n", a4.time, a4.iterations, e4, skipAfter);
+    auto a3 = pagerankMonolithicCuda(x, xt, init, {repeat, L1, 0, skipAfter});
+    auto e3 = l1Norm(a3.ranks, a1.ranks);
+    printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [skip-after=%d]\n", a3.time, a3.iterations, e3, skipAfter);
   }
 }
 
 
 int main(int argc, char **argv) {
   char *file = argv[1];
-  bool  show = argc > 2;
+  int repeat = argc>2? stoi(argv[2]) : 5;
   printf("Loading graph %s ...\n", file);
   auto x  = readMtx(file); println(x);
   auto xt = transposeWithDegree(x); print(xt); printf(" (transposeWithDegree)\n");
-  runPagerank(x, xt, show);
+  runPagerank(x, xt, repeat);
   printf("\n");
   return 0;
 }
